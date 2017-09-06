40150ea15a786654b1c35492b87cb09b72cbe1f2
Adrian Nicos <adrian.nicos@gmail.com>
2017-09-06 22:43:08 +0200

    bux fix

--- content_script.js
+++ content_script.js
@@ -26,7 +26,6 @@ function extractAndSaveNewMessages() {
     localStorage.setItem(getChannelId(), JSON.stringify(messages));
 }
 
-
 function getMessage(tsMessage){
     var timestamp = tsMessage.getAttribute("data-ts");
     var userName = tsMessage.getElementsByClassName("message_sender")[0].textContent;
@@ -41,11 +40,11 @@ function getMessage(tsMessage){
 
 function getChannelId() {
     var split = window.location.pathname.split("/");
-    return split[0]+split[1]+split[2];
+    return "history_"+split[0]+split[1]+split[2];
 }
 
 function addModalIfNotExist() {
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('historyModal');
     if (modal != null) {
         return;
     }
@@ -58,13 +57,34 @@ function addModalIfNotExist() {
     document.body.appendChild(inject);
 }
 
-function addHistoryButtonIfNotExist() {
+function sortAndConvertToArray(messages) {
+    var arrayMessages = [];
 
-    var historyButton = document.getElementById("history_actions_toggle")
-    if (historyButton != null) {
-        return;
+    for (var key in messages) {
+        arrayMessages.push(messages[key])
     }
 
+
+    arrayMessages.sort((function (a, b) {
+        if (a.timestamp < b.timestamp)
+            return -1;
+        if (a.timestamp > b.timestamp)
+            return 1;
+        return 0;
+    } ))
+    return arrayMessages;
+}
+function getDivHistory() {
+    var divHistory = document.getElementById('historyContentDiv');
+    return divHistory;
+}
+function clearHistoryView() {
+    var divHistory = getDivHistory();
+    while (divHistory.hasChildNodes()) {
+        divHistory.removeChild(divHistory.lastChild);
+    }
+}
+function injectHistoryButton() {
     var menuItems = document.getElementsByClassName("channel_title_info")[0];
     xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", chrome.extension.getURL("inject.html"), false);
@@ -73,36 +93,45 @@ function addHistoryButtonIfNotExist() {
     var inject = document.createElement("div");
     inject.innerHTML = xmlHttp.responseText;
     menuItems.appendChild(inject);
+}
+function addHistoryButtonIfNotExist() {
+
+    var historyButton = document.getElementById("history_actions_toggle")
+    if (historyButton != null) {
+        return;
+    }
+
+    injectHistoryButton();
 
 
     setTimeout(function () {
         document.getElementById("history_actions_toggle").onclick =function () {
-            console.log("ssss");
-            document.getElementById('myModal').style.display = "block";
-            var divHistory = document.getElementById('historyContentDiv');
-            var messages = JSON.parse(localStorage.getItem(getChannelId()));
-
-            for (var key in messages) {
-                var message = messages[key];
-                var divMessage = document.createElement("div");
-                divMessage.innerHTML = "<b>"+message.userName+"</b> <sup>"+message.timestamp+"</sup> <br/>"+message.messageText;
-                divHistory.appendChild(divMessage);
-            }
-
+            rendererHistoryView();
+            document.getElementById('historyModal').style.display = "block";
         };
 
         document.getElementsByClassName("close")[0].onclick = function(){
-            console.log("dup");
-            document.getElementById('myModal').style.display = "none";
-            while (divHistory.hasChildNodes()) {
-                divHistory.removeChild(divHistory.lastChild);
-            }
-
-
-           //
-           // message.innerHTML =
-
+            document.getElementById('historyModal').style.display = "none";
         };
-    },1000);
+    },2000);
+}
 
+function rendererHistoryView() {
+    clearHistoryView();
+    var divHistory = getDivHistory();
+    var messages = JSON.parse(localStorage.getItem(getChannelId()));
+    var arrayMessages = sortAndConvertToArray(messages);
+
+    for (var i in arrayMessages) {
+        var message = arrayMessages[i];
+        var divMessage = document.createElement("div");
+        var date = timestampToDate(message.timestamp);
+        divMessage.innerHTML = "<b>"+message.userName+"</b> <sup style='top: 0'>"+date+"</sup> <br/>"+"<div style='margin-top: 10px; margin-bottom: 10px;'>"+message.messageText+"</div>";
+        divHistory.appendChild(divMessage);
+    }
+}
+
+function timestampToDate(timestamp){
+    var date = new Date(timestamp * 1000);
+    return date.toGMTString();
 }
